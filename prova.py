2a8c0e6eb2a4865cbe539a029fedfd5f23026152
tali9943 <47156735+tali9943@users.noreply.github.com>
2023-04-13 17:29:33 +0200

    ranking #1

--- prova.py
+++ prova.py
@@ -1,17 +1,108 @@
-from sentence_transformers import SentenceTransformer
+import math
+import collections
+import nltk
+from collections import defaultdict
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
 
-#Our sentences we like to encode
-sentences = ['This framework generates embeddings for each input sentence',
-    'Sentences are passed as a list of string.',
-    'The quick brown fox jumps over the lazy dog.']
+def calculateIDF(tokenized_docs,term_freqs):
+    
+    #dictionary with the frequency of each term in all documents
+    all_terms = [term for doc in tokenized_docs for term in doc]
+    
+    df = collections.Counter(all_terms)
 
-#Sentences are encoded by calling model.encode()
-embeddings = model.encode(sentences)
+    # Calculate IDF for each term
+    N = len(tokenized_docs)
+    
+    idf = {}
+    
+    for term in df:
+        n = len([doc for doc in tokenized_docs if term in doc])
+        idf[term] = math.log(N / float(df[term] + 1)) #math.log(1 + (N - n + 0.5)/(n + 0.5))
+    return idf
 
-#Print the embeddings
-for sentence, embedding in zip(sentences, embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
-    print("")
+
+
+
+def calculateIDFBM25(tokenized_docs):
+    #dictionary with the frequency of each term in all documents
+    all_terms = [term for doc in tokenized_docs for term in doc]
+    
+    df = defaultdict(int)
+
+    # Calculate DF for each term
+    for term in all_terms:
+        df[term] += 1
+    # Calculate IDF for each term
+    N = len(tokenized_docs)
+    
+    idf = {}
+    
+    for term in df:
+        n = len([doc for doc in tokenized_docs if term in doc])
+        idf[term] = math.log((N - n + 0.5)/(n + 0.5)+1)
+    
+    return idf
+
+
+
+
+#idf = calculateIDFBM25(tokenized_docs)
+idf = calculateIDF(tokenized_docs,term_freqs)
+
+
+
+def calculateTF(tokenized_docs):
+    term_freqs = []
+    for doc in tokenized_docs:
+        doc_freq = collections.Counter(doc) #number of repetition for each word
+       
+        total_terms = len(doc) #length for each document
+        
+        for term in doc_freq:
+            doc_freq[term] /= float(total_terms)
+        term_freqs.append(doc_freq)
+
+    return term_freqs
+
+
+
+def calculateTF2(tokenized_docs):
+    term_freqs = []
+    for doc in tokenized_docs:
+        doc_freq = {}
+        total_terms = len(doc)
+        for term in doc:
+            doc_freq[term] = doc_freq.get(term, 0) + 1
+        for term in doc_freq:
+            doc_freq[term] /= float(total_terms)
+        term_freqs.append(doc_freq)
+
+    return term_freqs
+
+
+
+def calculateTFbm25(tokenized_docs):
+    term_freqs = []
+    k1 = 1.5 # parameter for controlling term frequency normalization
+    b = 0.75 # parameter for controlling document length normalization
+    avgdl = sum(len(doc) for doc in tokenized_docs) / len(tokenized_docs) # average document length
+
+    for doc in tokenized_docs:
+        doc_freq = collections.Counter(doc) #number of repetition for each word
+        total_terms = len(doc) #length for each document
+        doc_len_norm = ((1 - b) + b * (total_terms / avgdl)) # document length normalization factor
+
+        for term in doc_freq:
+            tf = doc_freq[term] / total_terms # term frequency
+            tf_norm = ((k1 + 1) * tf) / (k1 * ((1 - b) + b * (total_terms / avgdl)) + tf) # normalized term frequency with BM25 weighting
+            doc_freq[term] = tf_norm
+
+        term_freqs.append(doc_freq)
+
+    return term_freqs
+
+    
+#term_freqs = calculateTF2(tokenized_docs)
+term_freqs = calculateTFbm25(tokenized_docs)
+
